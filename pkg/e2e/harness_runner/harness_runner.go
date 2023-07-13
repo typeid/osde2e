872ac6640685d45868428a6c68f40bc0689fbd31
Ritu Mundhe <rmundhe@redhat.com>
2023-07-13 10:26:57 -0500

    templated runner

--- pkg/e2e/harness_runner/harness_runner.go
+++ pkg/e2e/harness_runner/harness_runner.go
@@ -12,8 +12,6 @@ import (
 	"github.com/openshift/osde2e/pkg/common/config"
 	"github.com/openshift/osde2e/pkg/common/helper"
 	"github.com/openshift/osde2e/pkg/common/label"
-	"github.com/openshift/osde2e/pkg/common/runner"
-	"github.com/openshift/osde2e/pkg/common/templates"
 	"github.com/openshift/osde2e/pkg/common/util"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -31,25 +29,22 @@ var _ = ginkgo.Describe("Test Harness", ginkgo.Ordered, label.TestHarness, func(
 		HarnessEntries = append(HarnessEntries, ginkgo.Entry("should run "+harness+" successfully", harness))
 	}
 	ginkgo.DescribeTable("Executing Harness",
-		func(harness string) {
+		func(ctx context.Context, harness string) {
 			ginkgo.By("======= RUNNING HARNESS: " + harness + " =======")
 			log.Printf("======= RUNNING HARNESS: %s =======", harness)
 			viper.Set(config.Project, "")
 			// Run harness in new project
 			h = helper.New()
-			h.SetServiceAccount(context.TODO(), "system:serviceaccount:%s:cluster-admin")
+			h.SetServiceAccount(ctx, "system:serviceaccount:%s:cluster-admin")
 			harnessImageIndex := strings.LastIndex(harness, "/")
 			harnessImage := harness[harnessImageIndex+1:]
 			suffix := util.RandomStr(5)
 			jobName := fmt.Sprintf("%s-%s", harnessImage, suffix)
-			r := h.RunnerWithNoCommand()
-			testCommand, err := getCommandString(h, harness, r, suffix, jobName)
-			Expect(err).NotTo(HaveOccurred(), "Could not create test pod template")
-			r.Cmd = testCommand
+			r := h.RunnerWithTemplateCommand(TimeoutInSeconds, harness, suffix, jobName, serviceAccountDir)
 
 			// run tests
 			stopCh := make(chan struct{})
-			err = r.Run(int(TimeoutInSeconds), stopCh)
+			err := r.Run(int(TimeoutInSeconds), stopCh)
 			Expect(err).NotTo(HaveOccurred(), "Could not run pod")
 
 			// get results
@@ -60,61 +55,11 @@ var _ = ginkgo.Describe("Test Harness", ginkgo.Ordered, label.TestHarness, func(
 			h.WriteResults(results)
 
 			// ensure job has not failed
-			_, err = h.Kube().BatchV1().Jobs(r.Namespace).Get(context.TODO(), jobName, metav1.GetOptions{})
+			_, err = h.Kube().BatchV1().Jobs(r.Namespace).Get(ctx, jobName, metav1.GetOptions{})
 			Expect(err).NotTo(HaveOccurred(), "Harness job pods failed")
 
-			h.Cleanup(context.TODO())
+			h.Cleanup(ctx)
 			ginkgo.By("======= FINISHED HARNESS: " + harness + " =======")
 		},
 		HarnessEntries)
 })
-
-// Generates templated command string to provide to test harness container
-func getCommandString(h *helper.H, harness string, r *runner.Runner, suffix string, jobName string) (string, error) {
-	// setup runner
-	latestImageStream, err := r.GetLatestImageStreamTag()
-	Expect(err).NotTo(HaveOccurred(), "Could not get latest imagestream tag")
-
-	values := struct {
-		Name                 string
-		JobName              string
-		Arguments            string
-		Timeout              int
-		Image                string
-		OutputDir            string
-		ServiceAccount       string
-		PushResultsContainer string
-		Suffix               string
-		Server               string
-		CA                   string
-		TokenFile            string
-		EnvironmentVariables []struct {
-			Name  string
-			Value string
-		}
-	}{
-		Name:                 jobName,
-		JobName:              jobName,
-		Timeout:              int(TimeoutInSeconds),
-		Image:                harness,
-		OutputDir:            runner.DefaultRunner.OutputDir,
-		ServiceAccount:       h.GetNamespacedServiceAccount(),
-		PushResultsContainer: latestImageStream,
-		Suffix:               suffix,
-		Server:               "https://kubernetes.default",
-		CA:                   serviceAccountDir + "/ca.crt",
-		TokenFile:            serviceAccountDir + "/token",
-		EnvironmentVariables: []struct {
-			Name  string
-			Value string
-		}{
-			{
-				Name:  "OCM_CLUSTER_ID",
-				Value: viper.GetString(config.Cluster.ID),
-			},
-		},
-	}
-	testTemplate, err := templates.LoadTemplate("tests/tests-runner.template")
-	Expect(err).NotTo(HaveOccurred(), "Could not load pod template")
-	return h.ConvertTemplateToString(testTemplate, values)
-}
